c96721abfbbf404a7fa43230cbf5c8f6fd0d0c3e
davidbasa <davidhutabarat47@gmail.com>
2021-09-26 00:16:57 +0700

    perbaikan table

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -1,4 +1,92 @@
 @extends('layouts.admin.template')
+@push('page-header', 'Edit Data User')
+
+@section('content')
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <h4 class="card-title">Edit Data User</h4>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="text-right">
+                            <a href="{{ route('user.index') }}" class="btn btn-outline-secondary"><i class="fas fa-arrow-left"></i> Kembali</a>
+                            <button type="button" class="btn btn-warning" id="btn-edit"><i class="fas fa-edit"></i> Edit</button>
+                            <button type="button" class="btn btn-danger" id="btn-reset" style="display: none"><i class="fas fa-times"></i> Reset</button>
+                            <button type="submit" class="btn btn-success" id="btn-submit" form="form-update" disabled><i class="fas fa-save"></i> Simpan</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <form action="/admin-area/user/{{$users->id}}" method="post" class="form-horizontal" id="form-update">
+                    @csrf
+                    @method('PUT')
+                    <div class="form-group row">
+                        <label class="col-lg-3 col-form-label">Nama User :</label>
+                        <div class="col-lg-5">
+                            <input type="text" class="form-control" name="name" maxlength="50" placeholder="Nama User " autocomplete="off" required value="{{$users->name}}" readonly>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-lg-3 col-form-label">Email :</label>
+                        <div class="col-lg-5">
+                            <input type="text" class="form-control" name="email" maxlength="50" placeholder="Email" autocomplete="off" required value="{{$users->email}}" readonly>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                    <label class="col-lg-3 col-form-label" for="role">Role :</label><br>
+                        <select class="col-lg-5" id="role" name="role">
+                            <option value="1" {{ $users->role_id == 1 ? 'selected="selected"' : '' }}>Admin</option>
+                            <option value="2" {{ $users->role_id == 2 ? 'selected="selected"' : '' }}>Donatur</option>
+                        </select>
+                    </div>
+                </form>
+            </div>
+        </div>
+        @if ($errors->any())
+            <div class="card">
+                <div class="card-body">
+                    <h5>Terdapat kesalahan: </h5>
+                    <div class="text-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        @endif
+    </div>
+@endsection
+
+@push('script')
+    <script>
+        $(document).ready(function (){
+            $('#btn-edit').on('click', function () {
+                $("input[name=name]").attr('readonly', false);
+                $("input[name=email]").attr('readonly', false);
+                $('#btn-edit').hide();
+                $('#btn-reset').show();
+                $('#btn-submit').attr('disabled', false);
+            });
+            $('#btn-reset').on('click', function () {
+                $("input[name=name]").attr('readonly', true);
+                $("input[name=name]").val('{{ $users->name }}');
+                $("input[name=email]").attr('readonly', true);
+                $("input[name=email]").val('{{ $users->email }}');
+                $('#btn-reset').hide();
+                $('#btn-edit').show();
+                $('#btn-submit').attr('disabled', true);
+            });
+        });
+    </script>
+@endpush
+
+
+<!-- @extends('layouts.admin.template')
 
 @section('content')
 
@@ -42,4 +130,4 @@
 </div>
 
 
-@endsection
+@endsection -->
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -15,9 +15,7 @@
 @section('content')
         <div class="col-lg-12">
         <div class="card">
-            <div class="card-header">
-                <h4 class="card-title">Data User</h4>
-            </div>
+
             <div class="card-body">
                 <div class="row mb-3">
                     <div class="offset-lg-6"></div>
@@ -109,43 +107,3 @@
     </script>
 @endpush
 
-
-<!-- 
-<div class="container">
-    <h3>List Users</h3>
-        <table class="table">
-            <thead class="thead-light">
-                <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nama User</th>
-                <th scope="col">Email</th>
-                <th scope="col">Role</th>
-                <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($roles as $key=>$value)
-                    <tr>
-                        <td>{{$key + 1}}</th>
-                        <td>{{$value->name}}</td>
-                        <td>{{$value->email}}</td>
-                        <td>{{$value->rn}}</td>
-                        <td>
-                            <a href="/admin-area/user/{{$value->id}}/edit" class="btn btn-primary">Edit</a>
-                            <form action="/game/{{$value->id}}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <input type="submit" class="btn btn-danger my-1" value="Delete">
-                            </form>
-                        </td>
-                    </tr>
-                @empty
-                    <tr colspan="3">
-                        <td>No data</td>
-                    </tr>  
-                @endforelse              
-            </tbody>
-        </table>
-
-
-</div> -->
