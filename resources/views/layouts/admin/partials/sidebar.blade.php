8262f7300496040f6326744c3f89c915f8ed081e
mesayusriana12 <mesayusriana12@gmail.com>
2021-09-25 22:32:10 +0700

    New : Logo dan Favicon

--- resources/views/layouts/admin/partials/sidebar.blade.php
+++ resources/views/layouts/admin/partials/sidebar.blade.php
@@ -2,7 +2,7 @@
 	<aside class="main-sidebar sidebar-dark-primary elevation-4">
 		<!-- Brand Logo -->
 		<a href="{{ route('admin.dashboard') }}" class="brand-link">
-			<img src="{{ asset('img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+			<img src="{{ asset('img/logo/logo.png') }}" alt="JBB Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
 			<span class="brand-text font-weight-light" style="font-size: 1.1rem">Jabar Bangkit Bersama</span>
 		</a>
 
